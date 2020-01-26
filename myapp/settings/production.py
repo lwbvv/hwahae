ae158df55b673368449711f3f563cda44cab624d
Ubuntu <ubuntu@ip-172-31-36-150.ap-northeast-2.compute.internal>
2020-01-26 17:36:09 +0000

    seconds test

--- myapp/settings/production.py
+++ myapp/settings/production.py
@@ -4,21 +4,9 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': '127.0.0.1',
-        'NAME': 'my_database',
-        'USER': 'root',
-        'PASSWORD': 'password',
+        'HOST': os.environ['MYSQL_ROOT_HOST'],
+        'NAME': os.environ['MYSQL_DATABASE'],
+        'USER': os.environ['MYSQL_USER'],
+        'PASSWORD': os.environ['MYSQL_ROOT_PASSWORD'],
     }
 }
-
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'HOST': os.environ['MYSQL_ROOT_HOST'],
-#         'NAME': os.environ['MYSQL_DATABASE'],
-#         'USER': os.environ['MYSQL_USER'],
-#         'PASSWORD': os.environ['MYSQL_ROOT_PASSWORD'],
-#     }
-# }
